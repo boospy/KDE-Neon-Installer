b043e44bc7274e04416c9138a82c0ba796870b72
Mario Loderer <mario.loderer@osit.cc>
2023-07-26 22:18:50 +0000

    Komplettes Update inkl. englischer Kommentare

--- kde-neon-22.04-installer.sh
+++ kde-neon-22.04-installer.sh
@@ -5,7 +5,7 @@
 # linux@osit.cc
 #
 
-# echo 'Acquire::http { Proxy "http://apt-cacher.osit.cc:3142"; };' | tee /etc/apt/apt.conf.d/01proxy
+#echo 'Acquire::http { Proxy "http://apt-cacher.osit.cc:3142"; };' | tee /etc/apt/apt.conf.d/01proxy
 rm /etc/apt/sources.list.d/preinstalled-pool.list
 
 gpg -k && gpg --no-default-keyring --keyring /usr/share/keyrings/iteas-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 23CAE45582EB0928
@@ -20,11 +20,13 @@ apt dist-upgrade -dy
 pkcon update -y
 
 # Hinzufügen von Repositories
+# Adding repositories
 apt-add-repository ppa:heyarje/makemkv-beta -n -y
 apt-add-repository ppa:papirus/papirus -n -y
 
 
 # UCS Univention Domänenanbindung
+# UCS Univention Domain Connection
 #add-apt-repository ppa:univention-dev/ppa -n -y
 
 gpg -k
@@ -35,11 +37,12 @@ echo "deb http://ppa.launchpadcontent.net/heyarje/makemkv-beta/ubuntu/ jammy mai
 apt update
 apt remove kwrite flatpak plasma-discover-backend-flatpak --purge -y
 
-apt install zsh ssh htop tree git kate kdf dolphin-nextcloud zsh nfs-common synaptic aspell-de hunspell-de-at mpv gnupg-agent kleopatra gnome-icon-theme mlocate kdepim-addons qt5-style-kvantum-themes materia-kde papirus-folders digikam akonadi-backend-sqlite korganizer showfoto kipi-plugins kde-config-cron dolphin-plugins filelight kcolorchooser soundkonverter kcalc partitionmanager kronometer kfind strawberry audacity unp kubuntu-restricted-extras katomic simplescreenrecorder avahi-utils tellico kio-stash language-pack-gnome-de finger onlyoffice-desktopeditors master-pdf-editor-5 gnome-disk-utility anydesk -y
+apt install zsh ssh htop tree git kate kdf dolphin-nextcloud zsh nfs-common synaptic aspell-de hunspell-de-at mpv gnupg-agent kleopatra gnome-icon-theme mlocate kdepim-addons qt5-style-kvantum-themes materia-kde papirus-folders digikam akonadi-backend-sqlite korganizer showfoto kipi-plugins kde-config-cron dolphin-plugins filelight kcolorchooser soundkonverter kcalc partitionmanager kronometer kfind strawberry audacity unp kubuntu-restricted-extras katomic simplescreenrecorder avahi-utils tellico kio-stash language-pack-gnome-de finger onlyoffice-desktopeditors master-pdf-editor-5 gnome-disk-utility rustdesk bitwarden -y
 
 
 # CD Brennen
-# apt install k3b k3b-extrathemes -y
+# Disk burning
+apt install k3b k3b-extrathemes -y
 
 ubuntu-drivers install
 
@@ -48,7 +51,7 @@ ubuntu-drivers install
 #dpkg -i google-chrome-stable_current_amd64.deb
 #apt install -fy
 
-# Brave Secure Browser (Installation nur ohne Apt-Proxy möglich)
+# Brave Secure Browser
 apt install curl -y
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] http://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -57,6 +60,7 @@ apt install brave-browser -y
 
 
 # Anpassen der viel zu niedrigen Werte in Sysctl
+# Adjusting the much too low values in Sysctl
 echo    "fs.file-max = 9223372036854775807" > /etc/sysctl.d/10-osit.conf
 echo "fs.inotify.max_user_instances = 1024"  >> /etc/sysctl.d/10-osit.conf
 echo "fs.inotify.max_user_watches = 5242880"  >> /etc/sysctl.d/10-osit.conf
@@ -64,6 +68,7 @@ echo "fs.inotify.max_user_watches = 5242880"  >> /etc/sysctl.d/10-osit.conf
 
 
 # Installation des UCS Clients für Kerberos/AD Anbindung an Univention Server
+# Installation of the UCS client for Kerberos/AD connection to Univention server
 #DEBIAN_FRONTEND=noninteractive apt-get install univention-domain-join krb5-auth-dialog -y
 
 echo "*.local" > /etc/mdns.allow
@@ -74,10 +79,11 @@ cp /usr/share/doc/avahi-daemon/examples/s* /etc/avahi/services/.
 systemctl restart avahi-daemon.service
 
 # ITEAS Mitarbeiter
-# apt install nload openfortigui openfortigui-runner virt-viewer hpmyroom pwgen konversation ldap-utils speedtest-cli filezilla filezilla-theme-papirus nmapsi4 realvnc-vnc-viewer smb4k mactelnet-client usb-creator-kde manpages-de preload krename kopano-deskapp kopano-deskapp-kdeintegration kompare wireshark kdenetwork-filesharing gtkterm samba xca libpam-mount davfs2 keyutils draw.io tinyotp -y
+# ITEAS Employees
+#apt install nload openfortigui openfortigui-runner virt-viewer hpmyroom pwgen konversation ldap-utils speedtest-cli filezilla filezilla-theme-papirus nmapsi4 realvnc-vnc-viewer smb4k mactelnet-client usb-creator-kde manpages-de preload krename kompare wireshark kdenetwork-filesharing gtkterm samba xca libpam-mount davfs2 keyutils draw.io tinyotp -y
 
-# optioanl ITEAS Developer Packages
-# apt install okteta manpages-de-dev php-mbstring composer dbeaver-ce -y
+# optional ITEAS Developer Packages
+#apt install okteta manpages-de-dev php-mbstring composer dbeaver-ce -y
 
 # ZSH-Shell
 apt update
@@ -94,28 +100,33 @@ tar -xf /tmp/nano.tar -C /root
 tar -xf /tmp/nano.tar -C /etc/skel
 rm /tmp/nano.tar -f
 
-# zshfix für Snaps
+# zshfix for Snaps
 echo "emulate sh -c 'source /etc/profile'" >> /etc/zsh/zprofile
 
 # komplette Kommunikationssuite Kontact, inkl. alle Plugins und Erweiterungen
-apt install accountsservice kdepim -y
+# complete communication suite Kontact, incl. all plugins and extensions
+#apt install accountsservice kdepim -y
 
 # optional Multimediapackages
-apt install openshot-qt mkvtoolnix-gui makemkv-bin kdenlive -y
+#apt install openshot-qt mkvtoolnix-gui makemkv-bin kdenlive -y
 
-# Teamviewer... Anydesk ist schon installiert
-# wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-# dpkg -i teamviewer_amd64.deb && apt install -fy
+# Teamviewer... Rustdesk ist schon installiert
+# Teamviewer... Rustdeskis already installed
+#wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+#dpkg -i teamviewer_amd64.deb && apt install -fy
 
+# Profi Fotobearbeitung
+# Professional photo editing
+#apt install gimp gimp-help-de darktable -y
 
 # optional Virtualbox LTS Version
-# apt install virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso -y
+#apt install virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso -y
 
 
 # Puppetanbindung bei Firma ITEAS IT Services Gmbh.
-# cd /root
-# wget https://git.styrion.net/iteas/iteas-tools/raw/master/puppet/ubuntu_puppet.sh
-# chmod +x ubuntu_puppet.sh
+#cd /root
+#wget https://git.styrion.net/iteas/iteas-tools/raw/master/puppet/ubuntu_puppet.sh
+#chmod +x ubuntu_puppet.sh
 
 apt remove timidity --purge -y
 apt autoremove --purge -y
